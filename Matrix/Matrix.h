e64ecaa6c8cc67a1d9cdcb7a90ec5d266d62dd99
RezaHemadi <r.hemadi@gmail.com>
2023-05-14 20:09:13 +0330

    Initial Commit

---
+++ Matrix/Matrix.h
@@ -0,0 +1,18 @@
+//
+//  Matrix.h
+//  Matrix
+//
+//  Created by Reza on 5/14/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Matrix.
+FOUNDATION_EXPORT double MatrixVersionNumber;
+
+//! Project version string for Matrix.
+FOUNDATION_EXPORT const unsigned char MatrixVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Matrix/PublicHeader.h>
+
+
